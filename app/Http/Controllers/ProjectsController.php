1a1e859c2cf77243784729f38f5415b5512ca26e
oussama reghay <oussamareghaye1998@gmail.com>
2021-11-30 19:11:30 +0100

    first commit

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public  function add_project(Request $request) {
+        $project = [
+            'content' =>$request->information,
+            'due' => $request->due,
+            'status' => $request->status,
+            'title' => $request->title,
+            'person' => $request->person
+        ];
+        Project::create($project);
+
+        return response()->json(['success' =>true, 'project' => $project]);
+    }
+
+
+    public function get_projects () {
+        $projects = Project::all();
+
+        return response()->json(['success' => true, 'projects' => $projects]);
+    }
+
+
+}
